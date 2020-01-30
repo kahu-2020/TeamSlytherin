e0fb3e88668e4f0d07632443f5fdbb47029100bf
mildredthecat <57421193+mildredthecat@users.noreply.github.com>
2020-01-31 08:27:12 +1300

    added images

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,7 +3,6 @@ import Wizard from './Wizard'
 
 const App = () => {
   return (
-
     <body>
       <h1>React development has begun!</h1>
     </body>
--- client/components/Wizard.jsx
+++ client/components/Wizard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-class extends Wizard = () => {
+const Wizard = () => {
   return (
     <div>
 
