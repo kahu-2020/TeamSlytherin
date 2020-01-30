223e9cc478383b745a4d647cff40703555bd2937
henry-harcourt <hnry.harcourt@gmail.com>
2020-01-31 11:57:04 +1300

    css cool

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,10 +4,15 @@ import Wizard from './Wizard'
 const App = () => {
   return (
     <div>
+
       <h1>Slytherin Spell Selector</h1>
-      <Wizard image="/image/malfoy.jpeg" name="Draco Malfoy" spell="Everte Statum" wand="Hawthorne wood and unicorn hair core" />
-      <Wizard image="image/snape.jpeg" name="Severus Snape" spell="Sectumsempra" wand="Oak and DRagon heart string" />
-      <Wizard image="/image/volde.jpeg" name="Lord Voldemort" spell="Avada Kedavra" wand="Yew and phoenix feather core" />
+
+      <div className="wizard-wrapper">
+        <Wizard image="/image/draco.png" name="Draco Malfoy" spell="Everte Statum" wand="Hawthorne wood and unicorn hair core" />
+        <Wizard image="/image/snape.png" name="Severus Snape" spell="Sectumsempra" wand="Oak and DRagon heart string" />
+        <Wizard image="/image/voldemort.png" name="Lord Voldemort" spell="Avada Kedavra" wand="Yew and phoenix feather core" />
+      </div>
+
     </div>
   )
 }
--- client/components/Wizard.jsx
+++ client/components/Wizard.jsx
@@ -5,22 +5,26 @@ class Wizard extends React.Component {
   constructor(props) {
     super(props)
 
-    
+
 
 
   }
-
 
   render() {
     console.log(this.props)
     return (
 
-      <div className='wizard' class="photo">
-        <img src={this.props.image} />
-        <h3> Name: {this.props.name}</h3>
-        <br />
-        <h3>Spell: {this.props.spell}</h3>
-        <h3>Wand: {this.props.wand}</h3>
+      <div className='wizard'>
+
+
+        <img className="image" src={this.props.image} />
+
+        <div className="text">
+
+          <h3> Name: {this.props.name}</h3>
+          <h3>Spell: {this.props.spell}</h3>
+          <h3>Wand: {this.props.wand}</h3>
+        </div>
 
       </div >
 
